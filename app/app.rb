a6d863a040693fc6bff3ba7920177cbd099327dd
Ian Fleming <>
2021-01-04 15:01:52 +0000

    web setup

---
+++ app/app.rb
@@ -0,0 +1,5 @@
+require 'sinatra/base'
+
+# class ClassName < Sinatra::Base    
+  run! if app_file == $0
+end
---
+++ app/config.ru
@@ -0,0 +1,3 @@
+require './app/app'
+
+# run Appname  (same as class name in app.rb and spec helper)
