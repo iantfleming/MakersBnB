e02985124a3ac686195732a9b25487e18b5bbf1d
Ara <hello.ara@pm.me>
2021-01-05 15:00:04 +0100

    add self.create method to listing model

--- spec/unit/listing_spec.rb
+++ spec/unit/listing_spec.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
 require 'listing'
+require 'database_helper'
 
-RSpec.describe 'Listing' do
-  it 'should have a name, price, description' do
-    listing = Listing.new('London', 100, 'Wonderful one-bedroom apartment with falling ceilings')
+describe 'Listing' do
+  # it 'should have a name, price, description' do   
+  # end
 
-    expect(listing.name).to eq 'London'
-    expect(listing.price).to eq 100
-    expect(listing.description).to eq 'Wonderful one-bedroom apartment with falling ceilings'
+  describe '.create' do
+    it 'creates a new listing' do
+      listing = Listing.create(name:'London', price: 100, description:'Wonderful one-bedroom apartment with falling ceilings')
+      persisted_data = persisted_data(id: listing.id, table: 'listings')
+      expect(listing).to be_a Listing
+      expect(listing.id).to eq persisted_data['id']
+      expect(listing.name).to eq 'London'
+      expect(listing.price).to eq 100
+      expect(listing.description).to eq 'Wonderful one-bedroom apartment with falling ceilings'
+    end
   end
 end
