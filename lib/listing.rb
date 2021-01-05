c4e0937b7edaa65e07c8f3cc220141bb3f35c8c9
Ara <hello.ara@pm.me>
2021-01-05 16:43:51 +0100

    update changes

--- lib/listing.rb
+++ lib/listing.rb
@@ -11,25 +11,25 @@ class Listing
   end
 
   def self.all
-    connection = if ENV['ENVIRONMENT'] == 'test'
-                   PG.connect(dbname: 'makersbnb_test')
-                 else
-                   PG.connect(dbname: 'makersbnb')
-                 end
+    if ENV['ENVIRONMENT'] == 'test'
+      con = PG.connect(dbname: 'makersbnb_test')
+    else
+      con = PG.connect(dbname: 'makersbnb')
+    end
 
-    result = connection.exec('SELECT * FROM listings')
+    result = con.exec('SELECT * FROM listings')
     result.map do |listing|
       Listing.new(id: listing['id'], name: listing['name'], price: listing['price'], description: listing['description'])
     end
   end
 
   def self.create(name:, price:, description:)
-    connection = if ENV['ENVIRONMENT'] == 'test'
-      PG.connect(dbname: 'makersbnb_test')
+    if ENV['ENVIRONMENT'] == 'test'
+      con = PG.connect(dbname: 'makersbnb_test')
     else
-      PG.connect(dbname: 'makersbnb')
+      con = PG.connect(dbname: 'makersbnb')
     end
-    result = connection.exec("INSERT INTO listings (name, price, description) VALUES ('#{name}', '#{price}', '#{description}') RETURNING id, name, price, description;")
+    result = con.exec("INSERT INTO listings (name, price, description) VALUES ('#{name}', '#{price}', '#{description}') RETURNING id, name, price, description;")
     Listing.new(id: result[0]['id'], name: result[0]['name'], price: result[0]['price'].to_i, description: result[0]['description'])
     
   end
