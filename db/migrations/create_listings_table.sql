ae2ff46224a96dd3cb0d52eca53ee37aab24c250
Ara <hello.ara@pm.me>
2021-01-07 15:56:25 +0100

    add dates to db

--- db/migrations/create_listings_table.sql
+++ db/migrations/create_listings_table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE listings(id SERIAL PRIMARY KEY, name TEXT NOT NULL, price NUMERIC DEFAULT 0, description TEXT NOT NULL, date date, available_from date, available_to date);
+CREATE TABLE listings(id SERIAL PRIMARY KEY, name TEXT NOT NULL, price NUMERIC DEFAULT 0, description TEXT NOT NULL, date text, available_from text, available_to text);
