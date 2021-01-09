0df9f5944146cf8b0a40d96d87cbdc6e7732524f
Luke Wickens <lukewickens@lukes-air.home>
2021-01-09 11:10:26 +0000

    Added create_bookings_table.sql and changed name on adding_image_column and edited error in create_user_table.sql.

---
+++ db/migrations/create_bookings_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY,
+    listing_id int4,
+    guest_email text,
+    host_email text,
+    status text,
+    dates_from text,
+    dates_to text,
+);
